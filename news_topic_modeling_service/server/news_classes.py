94f4860a5dbc30867cae3fa4d9599697492a0abc
Qyh <youknowqyh@gmail.com>
2021-07-15 15:57:00 +0800

    built and trained a CNN news topic classifer

---
+++ news_topic_modeling_service/server/news_classes.py
@@ -0,0 +1,19 @@
+classes = {
+    "Colleges & Schools", 
+    "Enviormental",
+    "World",
+    "Entertainment",
+    "Media",
+    "Politics & Government",
+    "Regional News",
+    "Religion",
+    "Sports",
+    "Technology",
+    "Traffic",
+    "Weather",
+    "Economic & Corp",
+    "Advertisements",
+    "Crime",
+    "Magazine",
+    "Other"
+}
