1355ab1f5f3919fdfebf01e3612510a5a94bc891
Qyh <youknowqyh@gmail.com>
2021-07-15 18:35:42 +0800

    fix some issue

--- web_server/server/app.js
+++ web_server/server/app.js
@@ -20,7 +20,6 @@ app.use('/static', express.static(path.join(__dirname, '../client/build/static/'
 
 
 
-
 // app.all('*', function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
 //   res.header("Access-Control-Allow-Headers", "X-Requested-With");
